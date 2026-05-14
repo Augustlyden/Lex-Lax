fb8f79ac0a0ceb51065a0e09f45c7973fe8cab6e
Augustlyden <august.lyden@outlook.com>
2026-05-14 17:31:15 +0200

    feat: implement express server

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import userRoutes from './routes/userRoutes.js';
+
+dotenv.config();
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({ origin: 'http://localhost:5173' }));
+
+app.use(express.json());
+
+app.get('/api/health', (req, res) => {
+  res.json({ success: true, message: 'Server is running' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT:${PORT}`)
+});
