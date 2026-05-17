d4b338b7f306edeb721fb502afdc509b5ad30feb
Augustlyden <august.lyden@outlook.com>
2026-05-17 15:35:29 +0200

    fix: recreated  missing files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
+import './styles/global.css'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
-
   return (
-    <>
-    </>
+  <>
+    <Navbar />
+
+    <main>
+      MAIN AREA
+    </main>
+
+    <Footer />
+  </>
   )
 }
 
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import "../styles/navbar.css"
+
+export default function Navbar() {
+
+    return (
+    <header>
+      <nav>
+        
+        <div className = "nav-container">
+          <div className = "nav-content">
+
+            <div className = "nav-logo">
+              <h1>LEX LÄX</h1>
+            </div>
+
+            <div className = "nav-links">
+              <a href="/">Home</a>
+              <a href="/">Support</a>
+              <a href="/">Contact</a>
+            </div>
+
+            <div className = "nav-profile">
+              <a href="/"> ⭐️</a>
+              <a href="/profile">👤</a>
+            </div>
+
+          </div>
+        </div>
+        
+      </nav>
+    </header>
+    )
+}
