4a7a5ef917513420449b8f6b1efd7248271873d9
Augustlyden <august.lyden@outlook.com>
2026-05-17 16:03:07 +0200

    fix: indentation to resemble develop branch

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,14 +1,12 @@
 import "../styles/footer.css"
 export default function Footer() {
     return (
-        
-         <footer className="footer">
-      <div className="footer-content">
-        <p>© 2026 Lex Läx</p>
-           By August, Sara, Emily & Kareem
-        <div className="footer-links">
-           </div>
-      </div>
-    </footer>
+      <footer className="footer">
+        <div className="footer-content">
+          <p>© 2026 Lex Läx</p>
+          By August, Sara, Emily & Kareem
+          <div className="footer-links"></div>
+        </div>
+      </footer>
     )
 }
