642e4a3befc6623fbb1af0e842d32a0bb1629e11
Sara Nylander <sara.nylander@hotmail.com>
2026-05-18 20:13:16 +0200

    feat: add Loading component and integrate it into App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import './styles/global.css'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
+import Loading from './components/Loading'
 
 function App() {
 
   return (
     <>
    <Navbar />
+
+   <Loading />
 
    <main>
     MAIN AREA
