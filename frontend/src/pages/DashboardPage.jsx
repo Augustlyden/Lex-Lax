f6301fdcec9c71d6174c596f798b9a33fec669db
Kareem D <drammeh@msn.com>
2026-05-18 18:09:34 +0700

    Added dashboardpage.jsx and styling

---
+++ frontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,67 @@
+import "../styles/dashboardPage.css"
+import { MdCalculate } from "react-icons/md";
+import { FaBook } from "react-icons/fa";
+import { IoTime } from "react-icons/io5";
+import { PiPencilSimpleFill } from "react-icons/pi";
+
+function DashboardPage() {
+ 
+    return (
+        <div className="dashboard-page-container">
+            <div className ="dashboard-content-wrapper">
+            
+            <div className = "info-box-wrapper">
+
+                <div className = "image-content-left">
+                    <img src="https://images.unsplash.com/photo-1503454537195-1dcabb73ffb9?q=80&w=500&auto=format&fit=crop" alt="Dashboard Illustration" />
+                </div>
+
+                <div className = "info-content-right">
+                <h2>Välkommen till din Dashboard!</h2>
+                <p>Här kan du se din studiehistorik, kommande uppgifter och anpassa dina inställningar.</p>
+                <div className="progress-bar">
+               <div className="progress-fill"></div>
+               </div>
+            </div>
+           </div>
+                <div className = "subject-container">
+                    
+                   
+                   <div className = "subject-card border-math">
+                    <div className="subject-icon-wrapper maths">
+                      <MdCalculate className="subject-icon" />
+                         </div>
+                        <h2>MATEMATIK</h2>
+                        <p>Du har 5 uppgifter kvar att slutföra.</p>
+                    </div>
+                       <div className = "subject-card border-glosor">
+                    <div className="subject-icon-wrapper glosor">
+                        <FaBook className="subject-icon" />
+                         </div>
+                        <h2>GLOSOR</h2>
+                        <p>Du har 5 uppgifter kvar att slutföra.</p>
+                    </div>
+                       <div className = "subject-card border-watch">
+                    <div className="subject-icon-wrapper watch">
+                        <IoTime className="subject-icon" />
+                         </div>
+                        <h2>KLOCKAN</h2>
+                        <p>Du har 5 uppgifter kvar att slutföra.</p>
+                    </div>
+                       <div className = "subject-card border-swedish">
+                    <div className="subject-icon-wrapper swedish">
+                       <PiPencilSimpleFill  className="subject-icon" />
+                         </div>
+                        <h2>SVENSKA</h2>
+                        <p>Du har 5 uppgifter kvar att slutföra.</p>
+                    </div>
+                </div>
+                         </div>
+                               </div>
+
+           
+                
+    )
+
+}
+export default DashboardPage;
