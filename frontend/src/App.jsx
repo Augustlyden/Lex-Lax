6933951e752fda68a2e4746a30e81b2b213a1724
Sara Nylander <sara.nylander@hotmail.com>
2026-05-19 13:19:17 +0200

    feat: add axios configuration and context provider for users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,29 @@ import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import './App.css'
 import LoginPage from "./pages/LoginPage"
+import { useAppContext } from './provider/ContextProvider.jsx';
+import Loading from './components/ui/Loading';
 
 function App() {
+
+  // ALLT HÄMTAS FRÅN CONTEXT PROVIDER, KAN ANVÄNDAS I HELA APPEN
+  const { currentUser, loading, error  } = useAppContext();
+  // ALLT HÄMTAS FRÅN CONTEXT PROVIDER, KAN ANVÄNDAS I HELA APPEN
+
   return (
   <>
     <Navbar />
+
+{/* TILLFÄLLIGT FÖR ATT VISA HUR DET KAN FUNKA :D  */}
+    {loading ? (
+      <Loading />
+    ) : currentUser ? (
+      <h1>Välkommen {currentUser.username}!</h1>
+    ) : (
+      <p>Error: {error}</p>
+    )}
+    {/* TILLFÄLLIGT FÖR ATT VISA HUR DET KAN FUNKA :D  */}
+
 
     <main>
   
---
+++ frontend/src/provider/ContextProvider.jsx
@@ -0,0 +1,56 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import axiosConfig from "../api/axiosConfig";
+
+const Context = createContext();
+
+export const ContextProvider = ({ children }) => {
+  const [users, setUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const errorHandler = (error) => {
+    setLoading(false);
+    setError(error.error);
+  }
+
+//   USERS
+  const fetchUsers = async () => {
+    try {
+        const data = await axiosConfig.get('/users/');
+        setUsers(data.data.data);
+        setLoading(false);
+    } catch (error) {
+        errorHandler(error);
+    }};
+
+  const fetchUsersById = async (id) => {
+    try {
+        const data = await axiosConfig.get(`/users/${id}`);
+        setCurrentUser(data.data.data);
+        setLoading(false);
+    } catch (error) {
+        errorHandler(error);
+    }};
+    
+    useEffect(() => {
+    fetchUsers();
+    // TILLFÄLLIGT HÅRDKODAT ID 
+    fetchUsersById(1);
+    },  []);
+
+  return (
+    <Context.Provider value={{ 
+        users, 
+        currentUser, 
+        loading, 
+        error 
+        }}>
+      {children}
+    </Context.Provider>
+  );
+};
+
+export const useAppContext = () => {
+  return useContext(Context);
+};
