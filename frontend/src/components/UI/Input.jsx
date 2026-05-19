7eb611b8f44e6c16393b2c8ef87a6d09751d89fd
Sara Nylander <sara.nylander@hotmail.com>
2026-05-19 10:48:39 +0200

    feat: add Input component and beginning aon css

---
+++ frontend/src/components/UI/Input.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../../styles/ui.css"
+
+const Input = ({ placeholder }) => {
+  return (
+    <input 
+    type="text" 
+    placeholder={placeholder} 
+    />
+  )
+}
+
+export default Input
