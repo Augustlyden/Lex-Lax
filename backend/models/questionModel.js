4383bcc58a80220162226b00c91ba656b11161ec
Augustlyden <august.lyden@outlook.com>
2026-05-20 12:14:07 +0200

    feat: add models for questions

---
+++ backend/models/questionModel.js
@@ -0,0 +1,60 @@
+import db from '../config/database.js';
+
+class Question {
+  static async findByListId(listId) {
+    const result = await db.query(`
+      SELECT 
+        questions.id,
+        questions.question,
+        questions.answer,
+        lists.title AS title,
+        lists.target_language AS language,
+        lists.created_at AS created
+      FROM questions
+      JOIN lists ON questions.list_id = lists.id
+      WHERE questions.list_id = $1
+      ORDER BY questions.id DESC;
+      `, [listId]
+    );
+    return result.rows;
+  }
+
+  static async create(listId, array) {
+    const values = [];
+    const valuePlaceholder = [];
+    let counter = 1;
+
+    for (const item of array) {
+      values.push(item.question, item.answer, listId);
+      valuePlaceholder.push(`($${counter}, $${counter + 1}, $${counter + 2})`);
+      counter += 3;
+    }
+
+    const result = await db.query(`
+      INSERT INTO questions (question, answer, list_id)
+      VALUES ${valuePlaceholder.join(', ')}
+      RETURNING *;
+      `, values
+    );
+
+    return result.rows;
+  }
+
+  static async update(question, answer, id) {
+    const result = await db.query(`
+      UPDATE questions
+      SET question = $1, answer = $2
+      WHERE id = $3
+      RETURNING *;
+      `, [question, answer, id]
+    );
+    return result.rows[0];
+  }
+
+  static async delete(id) {
+    const result = await db.query(
+      'DELETE FROM questions WHERE id = $1;', [id]
+    );
+    return result.rowCount > 0;
+  }
+}
