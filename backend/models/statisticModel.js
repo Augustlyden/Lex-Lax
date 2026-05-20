c304f3963a4f9259a9a2dd3005cc7090e4077d86
Augustlyden <august.lyden@outlook.com>
2026-05-20 15:43:19 +0200

    feat: add model for user_statistics

---
+++ backend/models/statisticModel.js
@@ -0,0 +1,54 @@
+import db from '../config/database.js';
+
+class Statistic {
+  static async findAll(userId) {
+    const result = await db.query(`
+      SELECT 
+        us.id,
+        us.correct_answers,
+        us.wrong_answers,
+        us.last_practiced,
+        lists.title AS title
+      FROM user_statistics us
+      JOIN lists ON us.list_id = lists.id
+      WHERE us.user_id = $1
+      ORDER BY us.last_practiced DESC;
+      `, [userId]
+    );
+    return result.rows;
+  }
+
+  static async findByListId(userId, listId) {
+    const result = await db.query(`
+      SELECT
+        us.id,
+        us.correct_answers,
+        us.wrong_answers,
+        us.last_practiced,
+        lists.title AS title
+      FROM user_statistics us
+      JOIN lists ON us.lists_id = lists.id
+      WHERE us.user_id = $1 AND us.list_id = $2;
+      `, [userId, listId]
+    );
+    return result.rows[0];
+  }
+
+  static async upsert(userId, listId, correctCount, wrongCount) {
+    const result = await db.query(`
+      INSERT INTO user_statistics 
+        (user_id, list_id, correct_answers, wrong_answers, last_practiced)
+      VALUES ($1, $2, $3, $4, NOW())
+      ON CONFLICT (user_id, list_id)
+      DO UPDATE SET
+        correct_answers = user_statistics.correct_answers + EXCLUDED.correct_answers,
+        wrong_answers = user_statistics.wrong_answers + EXCLUDED.wrong_answers,
+        last_practiced = NOW()
+      RETURNING *;
+      `, [userId, listId, correctCount, wrongCount]
+    );
+    return result.rows[0];
+  }
+}
+
+export default Statistic;
