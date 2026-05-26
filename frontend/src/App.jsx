60aa85087fcfadcd47855eccfb3f7f79775425a9
Emily Eriksson <emily.eriksson@gmail.com>
2026-05-26 11:05:52 +0200

    Resolve merge conflicts in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,59 +1,55 @@
-import './styles/global.css'
-import './App.css'
-import './styles/buttons.css'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
-import LoginPage from "./pages/LoginPage"
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import QuestionPage from './pages/QuestionPage'
-import DashboardPage from "./pages/DashboardPage"
-import VocabularyPage from './pages/VocabularyPage'
-import AddUser from './pages/AddUser'
-import { PureComponent } from 'react'
-import CreateWordList from './components/CreateWordList'
-import EditWordList from "./components/EditWordList"
-import MathPage from "./pages/MathPage"
-import StatisticPage from './pages/StatisticPage'
+import "./styles/global.css";
+import "./App.css";
+import "./styles/buttons.css";
+
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+import { Routes, Route } from "react-router-dom";
+
+import LoginPage from "./pages/LoginPage";
+import QuestionPage from "./pages/QuestionPage";
+import DashboardPage from "./pages/DashboardPage";
+import VocabularyPage from "./pages/VocabularyPage";
+import AddUser from "./pages/AddUser";
+import CreateWordList from "./components/CreateWordList";
+import EditWordList from "./components/EditWordList";
+import MathPage from "./pages/MathPage";
+import StatisticPage from "./pages/StatisticPage";
 
 function App() {
-
   return (
     <>
+      <Navbar />
 
-
-      <BrowserRouter>
-        <Navbar />
-
-        <main>
+      <main>
         <Routes>
-          <Route path="/" element={<DashboardPage />} />
+          <Route path="/" element={<LoginPage />} />
+
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/logga-in" element={<LoginPage />} />
+
+          <Route path="/add-user" element={<AddUser />} />
           <Route path="/skapa-profil" element={<AddUser />} />
+          <Route path="/edit-user/:userId" element={<AddUser />} />
+
+          <Route path="/dashboard/:userId" element={<DashboardPage />} />
+
           <Route path="/test" element={<QuestionPage />} />
           <Route path="/amne" element={<VocabularyPage />} />
 
-          <Route path="/vocabulary/:subjectId" element={<VocabularyPage />}/>
-          <Route path="/math/:subjectId" element={<MathPage />}/>
-          <Route path="/vocabulary/:subjectId/create" element={<CreateWordList />}/>
-          <Route path="/vocabulary/:subjectId/edit/:listId" element={<EditWordList />}/>
+          <Route path="/vocabulary/:subjectId" element={<VocabularyPage />} />
+          <Route path="/math/:subjectId" element={<MathPage />} />
+          <Route path="/vocabulary/:subjectId/create" element={<CreateWordList />} />
+          <Route path="/vocabulary/:subjectId/edit/:listId" element={<EditWordList />} />
 
-          <Route path='/statistik' element={<StatisticPage />}></Route>
-
+          <Route path="/statistik" element={<StatisticPage />} />
         </Routes>
+      </main>
 
-        </main>
-    <main>
-       < LoginPage/>
- 
-  </main>
-
-    
-
-
-        <Footer />
-      </BrowserRouter>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
